04ed84c3f882998b9d977eb406ff43d78a893724
mor-c1 <myeremychev@mail.ru>
2026-04-29 23:52:39 +0300

    ex2
    67

---
+++ ex2v2/ex2v2/ex2v2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main() {
+    const int ROWS = 100;
+    const int COLS = 200;
+
+    int** arr = new int* [ROWS];
+
+    for (int i = 0; i < ROWS; i++)
+        arr[i] = new int[COLS];
+
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
+            arr[i][j] = -3 + (i * COLS + j) % 14;
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++)
+            std::cout << arr[i][j] << " ";
+        std::cout << "\n";
+    }
+
+    for (int i = 0; i < ROWS; i++)
+        delete[] arr[i];
+    delete[] arr;
+
+    return 0;
+}
