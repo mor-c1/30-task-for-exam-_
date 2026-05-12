3e405ae2f5c789f6316f84e34bf67837fbc016e4
mor-c1 <myeremychev@mail.ru>
2026-05-13 01:03:38 +0300

    ex22
    67

---
+++ ex22/ex22/ex22.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <ctime>
+
+double getInterval() {
+    std::cout << "Нажмите Enter...";
+    std::cin.get();
+    clock_t start = clock();
+
+    std::cout << "Нажмите Enter снова...";
+    std::cin.get();
+    clock_t end = clock();
+
+    return (double)(end - start) / CLOCKS_PER_SEC;
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+
+    double interval = getInterval();
+    std::cout << "Интервал времени: " << interval << " секунд\n";
+
+    return 0;
+}
